19d6d2bd367e2c568df8c55f3b8355078f4519df
crespire <li.simmon@gmail.com>
2022-03-11 11:25:01 -0500

    Update permissions to use its own tracked YAML file.

--- bot.rb
+++ bot.rb
@@ -2,13 +2,14 @@ require 'discordrb'
 require 'yaml'
 
 CONFIG = YAML.load_file('config.yaml')
+PERMISSIONS = YAML.load_file('permissions.yaml')
 bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'], client_id: 951501962585726977, prefix: '>'
 puts "Bot invite link: #{bot.invite_url}"
-puts "Bot active channels: #{CONFIG['bot-channels']}"
-puts "Role IDs allowed for request: #{CONFIG['allow-request']}"
-puts "Role IDs allowed for manage: #{CONFIG['allow-manage']}"
+puts "Bot active channels: #{PERMISSIONS['bot-channels']}"
+puts "Role IDs allowed for request: #{PERMISSIONS['allow-request']}"
+puts "Role IDs allowed for manage: #{PERMISSIONS['allow-manage']}"
 
-bot.command(:test_permission, allowed_roles: CONFIG['allow-manage'], channels: CONFIG['bot-channels'], description: 'Command to test permission setting.') do |event|
+bot.command(:test_permission, allowed_roles: PERMISSIONS['allow-manage'], channels: PERMISSIONS['bot-channels'], description: 'Command to test permission setting.') do |event|
   event.respond 'Permission check passed!'
 end
 
@@ -16,7 +17,7 @@ bot.command(:meta, help_available: false) do |event|
   event.respond 'You found the hidden command!'
 end
 
-bot.command(:request, allowed_roles: CONFIG['allow-request'], channels: CONFIG['bot-channels'], description: 'Takes a request and creates a ticket to be resolved. Request multiple items by separating `[number][name]` with a comma.', usage: 'request [number][item name/description](, [number][name of additional items])') do |event, *args|
+bot.command(:request, allowed_roles: PERMISSIONS['allow-request'], channels: PERMISSIONS['bot-channels'], description: 'Takes a request and creates a ticket to be resolved. Request multiple items by separating `[number][name]` with a comma.', usage: 'request [number][item name/description](, [number][name of additional items])') do |event, *args|
   request = args.join(' ')
 
   pairs = []
